a85914f88e3b29b4e265ef893e577a0ada0546d6
Agasthya1210 <134372742+Agasthya1210@users.noreply.github.com>
2023-05-23 17:03:40 +0530

    Delete validnumber.py

--- validnumber.py
+++
@@ -1,6 +0,0 @@
-number=int(input('Enter your mobile number: '))
-mobilenum=str(number)
-if len(mobilenum)==10:
-    print('valid mobile number: '+ str(number))
-else:
-    print('invalid mobile number')
